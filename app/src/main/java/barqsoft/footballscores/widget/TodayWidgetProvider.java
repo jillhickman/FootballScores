2e68f82e462a26613d6223ac1d3dd30b04ee4426
Jill Hickman <jillhickman@gmail.com>
2015-09-30 21:25:01 -0600

    Info in widget

--- app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
@@ -5,10 +5,17 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.widget.RemoteViews;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
 
 /**
  * Created by jillhickman on 9/29/15.
@@ -51,6 +58,40 @@ public class TodayWidgetProvider extends AppWidgetProvider {
         PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
 
         remoteViews.setOnClickPendingIntent(R.id.widget, configPendingIntent);
+
+//        Date d = new Date();
+//        String date = (new SimpleDateFormat("yyyy-MM-dd")).format(d);
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -2);
+        String myDate = dateFormat.format(calendar.getTime());
+
+        String [] SCORES_COLUMNS = {
+                DatabaseContract.scores_table.HOME_COL,
+                DatabaseContract.scores_table.AWAY_COL,
+                DatabaseContract.scores_table.HOME_GOALS_COL,
+                DatabaseContract.scores_table.AWAY_GOALS_COL,
+                DatabaseContract.scores_table.TIME_COL,
+        };
+
+        Cursor data = context.getContentResolver().query(DatabaseContract.scores_table.buildScoreWithDate(),
+                SCORES_COLUMNS,
+                null,
+                new String[]{myDate},
+//              null,
+                DatabaseContract.scores_table.HOME_GOALS_COL + " DESC LIMIT 1");
+
+        int j = data.getCount();
+
+        if (data != null && data.moveToFirst()) {
+            remoteViews.setTextViewText(R.id.widget_home_name, data.getString(0));
+            remoteViews.setTextViewText(R.id.widget_away_name, data.getString(1));
+            remoteViews.setTextViewText(R.id.widget_score_textview, Utilies.getScores(data.getInt(2), data.getInt(3)));
+        }
+//        mHolder.away_name.setText(data.getString(scoresAdapter.COL_AWAY));
+//        mHolder.score.setText(Utilies.getScores(data.getInt(scoresAdapter.COL_HOME_GOALS), data.getInt(scoresAdapter.COL_AWAY_GOALS)));
+
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
 }
